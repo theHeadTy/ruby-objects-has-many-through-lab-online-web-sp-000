a4457162de6dcad6a400d4178f0de90fda5daeac
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 18:46:44 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,13 @@
 class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+  
+  def self.all
+    @@all
+  end
 end
