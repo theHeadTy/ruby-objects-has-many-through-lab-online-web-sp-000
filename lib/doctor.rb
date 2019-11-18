852bda1f6be86feb17761da6172c986dbfddbbe0
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 18:53:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,6 +11,7 @@ class Doctor
   end
   
   def appointments
+    Appointment.all.select { |app| app.doctor == self }
   end
   
   def new_appointment(patient, date)
@@ -21,4 +22,39 @@ class Doctor
   end 
     
   
+end
+
+  describe '#appointments' do
+    it 'returns all appointments associated with this Doctor' do
+      doctor_who = Doctor.new('The Doctor')
+      doctor_smith = Doctor.new('Matt Smith')
+      hevydevy = Patient.new('Devin Townsend')
+      appointment = Appointment.new('Friday, January 32nd', hevydevy, doctor_who)
+      appointment_2 = Appointment.new('Saturday, January 33rd', hevydevy, doctor_smith)
+      
+      expect(doctor_who.appointments).to include(appointment)
+      expect(doctor_smith.appointments).to include(appointment_2)
+
+    end
+  end
+
+  describe '#new_appointment' do
+    it 'given a date and a patient, creates a new appointment' do
+      doctor_who = Doctor.new('The Doctor')
+      hevydevy = Patient.new('Devin Townsend')
+      appointment = doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
+      expect(doctor_who.appointments).to include(appointment)
+      expect(appointment.doctor).to eq(doctor_who)
+    end
+  end
+
+  describe '#patients' do
+    it 'has many patients, through appointments' do
+      doctor_who = Doctor.new('The Doctor')
+      hevydevy = Patient.new('Devin Townsend')
+      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
+
+      expect(doctor_who.patients).to include(hevydevy)
+    end
+  end
 end
