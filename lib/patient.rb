8027004cd61580a873f628e02a0c4c14c76dc3ab
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 18:50:14 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,52 @@
 class Patient
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select { |app| app.patient == self }
+  end
+  
+  def doctors
+    Doctor.all.map { |doc| doc.
+end
+
+
+
+  describe '#appointments' do
+    it 'returns all appointments associated with this Patient' do
+      doctor_who = Doctor.new('The Doctor')
+      doctor_smith = Doctor.new('Matt Smith')
+      steve = Patient.new('Steve')
+      mike = Patient.new('Mike')
+      appointment = Appointment.new('Friday, January 32nd', steve, doctor_who)
+      appointment_2 = Appointment.new('Saturday, January 33rd', steve, doctor_smith)
+      appointment_3 = Appointment.new('Sunday, January 34th', mike, doctor_who)
+
+      expect(steve.appointments).to include(appointment)
+      expect(steve.appointments).to include(appointment_2)
+      expect(steve.appointments).to_not include(appointment_3)
+      expect(mike.appointments).to include(appointment_3)
+    end
+  end
+
+  describe '#doctors' do
+    it 'has many doctors through appointments' do
+      doctor_who = Doctor.new('The Doctor')
+      doctor_what = Doctor.new('Das Doktor')
+      hevydevy = Patient.new('Devin Townsend')
+      hevydevy.new_appointment(doctor_who, 'Friday, January 32nd')
+      hevydevy.new_appointment(doctor_what, 'Saturday, January 32nd')
+
+      expect(hevydevy.doctors).to include(doctor_who)
+      expect(hevydevy.doctors).to include(doctor_what)
+    end
+  end
 end
