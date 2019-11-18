1d873695084d635f1d9d4c844b761f808cd88b96
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 18:54:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,15 +22,3 @@ class Doctor
     appointments.map { |app| app.patient }
   end 
 end
-
-
-  describe '#patients' do
-    it 'has many patients, through appointments' do
-      doctor_who = Doctor.new('The Doctor')
-      hevydevy = Patient.new('Devin Townsend')
-      doctor_who.new_appointment(hevydevy, 'Friday, January 32nd')
-
-      expect(doctor_who.patients).to include(hevydevy)
-    end
-  end
-end
